c8245487c72fb033b83f68dbf2f2c890e319a1c3
Rajit kumar <krajit909@gmail.com>
2024-01-17 21:23:30 +0530

    conditional in  react

--- src/component/producttab.jsx
+++ src/component/producttab.jsx
@@ -2,12 +2,19 @@ import React from 'react'
 import Product from './product'
 
 const Producttab = () => {
+  let features = [
+    "fast",
+    "smart",
+    "durable"
+  ]
+
+ 
   return (
     <div className='main'>
       <h3>Prioducts</h3>
-      <Product title="Title" price="30"/>
-      <Product title="Title" price="50"/>
-      <Product title="Title" price="60"/>
+      <Product title="Phone" />
+      <Product title="Laptop" price={12999} features={features }/>
+      <Product title="mouse" price={299} />
     </div>
   )
 }
