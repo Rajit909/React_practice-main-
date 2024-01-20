864d934fcddfb45f273604a35ab48ae956a97ed3
Rajit kumar <krajit909@gmail.com>
2024-01-20 20:57:49 +0530

    product fetching

--- src/component/product.jsx
+++ src/component/product.jsx
@@ -1,26 +1,47 @@
-import React from 'react'
-import Price from './Price'
+import React from "react";
+import Price from "./Price";
 
-function Product({title, price=5999, features}){
-
-  // const list = features ? features.map((feature, index) => 
+function Product({ title, desc, idx }) {
+  // const list = features ? features.map((feature, index) =>
   //   <li key={index}>{feature}</li>) : null;
 
-   
+  let titleName = [
+    "Oppo realme 10",
+    "Oppo realme 9",
+    "Oppo realme 7",
+    "Oppo realme 8",
+  ];
+  let oldPrice = [
+    "2999",
+    "3999",
+    "1599",
+    "2099"
+  ];
+  let newPrice = [
+    "2399",
+    "3000",
+    "1299",
+    "1599"
+  ];
+  let desciption = [
+    "Ele",
+    "Ele",
+    "Ele",
+    "Ele"
+  ];
   return (
     <>
-    <div className="card">
-      <div className="head">
-
-    <h3>{title}</h3>
+      <div className="card">
+        <div className="head">
+          <h4>{titleName[idx]}</h4>
+          <p>{desciption[idx]}</p>
+        </div>
+        <div className="price">
+          <Price oldPrice={oldPrice[idx]} newPrice={newPrice[idx]} />
+        </div>
       </div>
-    <div className="price">
-     <Price oldPrice={5999} newPrice={4999}/>
-    </div>
-    </div>
     </>
-
-  )
+  );
 }
 
-export default Product
+export default Product;
--- src/component/producttab.jsx
+++ src/component/producttab.jsx
@@ -2,16 +2,16 @@ import React from 'react'
 import Product from './product'
 
 const Producttab = () => {
-
+  
   return (
     <>
     <div className='container'>
       <h4>Prioducts</h4>
       <div className='main'>
-        <Product title="Product"/>
-        <Product title="Product"/>
-        <Product title="Product"/>
-        <Product title="Product"/>
+        <Product title="Product" idx={0}/>
+        <Product title="Product" idx={1}/>
+        <Product title="Product" idx={2}/>
+        <Product title="Product" idx={3}/>
       </div>
     </div>
     </>
