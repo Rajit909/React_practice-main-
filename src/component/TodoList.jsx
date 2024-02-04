0c756b9eab275ed75e2ae658a3322438b90def17
Rajit kumar <krajit909@gmail.com>
2024-02-04 22:19:06 +0530

    bug fix

--- src/component/TodoList.jsx
+++ src/component/TodoList.jsx
@@ -1,17 +1,17 @@
 import React from "react";
 import { useState } from "react";
-import {v4 as uuidv4} from "uuid";
+import { v4 as uuidv4 } from "uuid";
 
 function TodoList() {
-  let [todos, setTodos] = useState([{task: "sample task", id: uuidv4() }]);
+  let [todos, setTodos] = useState([{ task: "sample task", id: uuidv4() }]);
   let [newTodo, setNewTodo] = useState([""]);
 
   let addNewTodo = () => {
     console.log("new");
-    setTodos((prevTodos)=>{
-        return [...prevTodos, {task: newTodo, id: uuidv4()}]
-    })
-    setNewTodo("")
+    setTodos((prevTodos) => {
+      return [...prevTodos, { task: newTodo, id: uuidv4() }];
+    });
+    setNewTodo("");
   };
 
   let updateTodoValue = (event) => {
@@ -19,20 +19,46 @@ function TodoList() {
   };
 
   let delTask = (id) => {
-    setTodos(todos.filter((todo) => todo.id !== id))
-
-  }
+    setTodos(todos.filter((todo) => todo.id !== id));
+  };
 
   let upperCaseAll = () => {
-    setTodos((prevTodos) => (
-        prevTodos.map((todo) => {
+    setTodos((prevTodos) =>
+      prevTodos.map((todo) => {
         return {
-            ...todo,
-            task: todo.task.toUpperCase()
+          ...todo,
+          task: todo.task.toUpperCase(),
+        };
+      })
+    );
+  };
+
+  // let lowerCaseAll = () => {
+  //   setTodos((prevTodos) =>
+  //     prevTodos.map((todo) => {
+  //       return {
+  //         ...todo,
+  //         task: todo.task.toLowerCase(),
+  //       };
+  //     })
+  //   );
+  // };
+
+  let lowerCaseone = (id) => {
+    setTodos((prevTodos) => 
+      prevTodos.map((todo) => {
+        if(todo.id === id){
+        return {
+          ...todo,
+          task: todo.task.toLowerCase()
         }
-    })))
-    
-  } 
+        }else{
+          return todo;
+        }
+      })
+    )
+  }
+
   return (
     <>
       <h4>Todo-list</h4>
@@ -60,20 +86,26 @@ function TodoList() {
           <br />
           <br />
           <br />
-          <br />
           <hr />
           <h4>Tasks to do</h4>
           <ul>
             {todos.map((todo) => (
               <li key={todo.id}>
                 <span>{todo.task}</span>
-                <button id="taskbtn"
-                onClick={() => delTask(todo.id)}
-                >❌</button>
+                <button id="taskbtn" onClick={() => delTask(todo.id)}>
+                  ❌
+                </button>
+                <button id="taskbtn" onClick={lowerCaseone}>
+                  LC
+                </button>
               </li>
-              
-              ))}
-              <button id="taskbtn" onClick={upperCaseAll}>Update</button>
+            ))}
+            <button id="taskbtn" onClick={upperCaseAll}>
+              UpperCase
+            </button>
+            {/* <button id="taskbtn" onClick={lowerCaseAll}>
+              lowerCase
+            </button> */}
           </ul>
         </div>
       </div>
