fc0f304812bce05ec5ce467389f12567533e1dd5
Rajit kumar <krajit909@gmail.com>
2024-02-07 22:20:57 +0530

    bug fix

--- src/component/lottery game/Lottery.jsx
+++ src/component/lottery game/Lottery.jsx
@@ -1,32 +1,19 @@
 import React, { useState } from 'react'
 import "./Lottery.css"
-import {genTicket, sum} from "./helper"
+import Ticketnum from "./Ticketnum"
 
 function Lottery() {
-    let [ticket, setTicket] = useState(genTicket(3));
-    let isWinning = sum(ticket) === 15
     
-    let buyTicket = () => {
-        setTicket(genTicket(3))
-    }
 
   return (
     <>
-    <div className='center'>
-    <h1>Lottery Game!</h1>
-    <div className='ticket-container'>
-    <span>{ticket[0]}</span>
-    <span>{ticket[1]}</span>
-    <span>{ticket[2]}</span>
+    <div className='main-continer'>
+        <div>
+    <Ticketnum num={2}/>    
+    <Ticketnum num={2}/>    
+    <Ticketnum num={2}/>    
+        </div>
     </div>
-    <button onClick={buyTicket}>Buy more ticket</button>
-    <h3 style={{
-        backgroundColor: "yellow"
-    }}>{isWinning && "Congraltulation, You won!"}
-    </h3>
-    <p>{sum(ticket)}</p>
-    </div>
-    
     </>
   )
 }
---
+++ src/component/lottery game/Ticket.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Ticket({ticket}) {
+  return (
+    <div>Ticket</div>
+  )
+}
