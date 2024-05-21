e130f102912c5a5c9699686fce80c964a4e97300
Rajit Kumar <94344369+Rajit909@users.noreply.github.com>
2024-05-21 16:27:08 +0530

    Update App

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,21 @@
 import React from 'react'
 import './App.css'
 import CommentsForm from './component/CommentsForm'
-// import Lottery from './component/lottery game/Lottery'
-// import TodoList from './component/TodoList'
-// import LudoBoard from './component/LudoBoard'
-// import LikeButton from './component/LikeButton'
-// import Counter from './component/Counter'
-// import Producttab from './component/producttab'
-// import Button from './component/Button'
-// import Form from './component/Form'
+ import Lottery from './component/lottery game/Lottery'
+import TodoList from './component/TodoList'
+import LudoBoard from './component/LudoBoard'
+import LikeButton from './component/LikeButton'
+import Counter from './component/Counter'
+import Producttab from './component/producttab'
+ import Button from './component/Button'
+ import Form from './component/Form'
 
 
 
 function App() {
-  // let winCondition = (ticket) =>{
-  //   return ticket.every((num) => num === ticket[0])
-  // }
+  let winCondition = (ticket) =>{
+     return ticket.every((num) => num === ticket[0])
+  }
   return (
     <>
        <Form/> 
